2e8603e84f1b6d5eff31622159fce02ae6507813
amarudemonn <amarudemonn@gmail.com>
2021-01-05 17:10:49 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+let map;
+
+function initMap() {
+  const loc = { lat: 39.584265433518915, lng: -74.31066565197669 }
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: loc,
+    zoom: 14,
+  });
+
+  const marker = new google.maps.Marker({
+    position: loc,
+    map: map
+  });
+}
+
+$('#main-header a').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top - 100
+    }, 800);
+  }
+});
