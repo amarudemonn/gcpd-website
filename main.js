fd050da7cf350dfee4a05ad8a54784fdedce69c0
Akari Amano <37510266+amarudemonn@users.noreply.github.com>
2021-01-06 15:15:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function initMap() {
 
   const map = new google.maps.Map(document.getElementById("map"), {
     center: loc,
-    zoom: 8,
+    zoom: 4,
   });
 
   const marker = new google.maps.Marker({
